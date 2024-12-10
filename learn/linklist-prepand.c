ab2bc13b581696edd50175c5b5574389577eef6b
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-12-10 15:54:26 +0630

    feat(linklist): prepanding exe

---
+++ learn/linklist-prepand.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <sys/_types/_null.h>
+
+// cannot use binary search algorithm in linklist
+
+typedef struct node {
+  int number;
+  struct node *next;
+} node;
+
+int main(int argc, char *argv[]) {
+  node *list = NULL;
+
+  // prepanding
+  for (int i = 1; i < argc; i++) {
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+      free(list);
+      printf("cannot take memmory location for temp\n");
+      return 1;
+    }
+
+    n->number = atoi(argv[i]);
+    n->next = list;
+    list = n;
+  }
+
+  node *ptr = list;
+  while (ptr != NULL) {
+    printf("%i\n", ptr->number);
+    ptr = ptr->next;
+  }
+
+  free(ptr);
+  free(list);
+  return 0;
+}
